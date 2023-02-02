e2f4cfb3965cc8fe4d8cc156c34b74f57497887e
汪志鹏 <13476859295@163.com>
2023-02-02 14:15:38 +0800

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -20,7 +20,7 @@ var render = Render.create({
 // create two boxes and a ground
 var boxA = Bodies.rectangle(400, 200, 80, 80);
 var boxB = Bodies.rectangle(450, 50, 80, 80);
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+var ground = Bodies.rectangle(400, 610, 810, 200, { isStatic: true });
 
 // add all of the bodies to the world
 Composite.add(engine.world, [boxA, boxB, ground]);
