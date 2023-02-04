1ce0805049097a511f749f7dcbe000a37bed2c92
汪志鹏 <13476859295@163.com>
2023-02-04 22:52:55 +0800

    add Bullet

---
+++ src/scripts/roles/prototype/Bullet.js
@@ -0,0 +1,12 @@
+class Bullet {
+    constract(x, y, directionX, directionY, Bodies) {
+        this.x = x;
+        this.y = y;
+        this.directionX = directionX;
+        this.directionY = directionY;
+        this.radius = 10;
+        this.body = Bodies.circle(this.x, this.y, this.radius, {
+            restitution:0
+        });
+    }
+}
